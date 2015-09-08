3bb118504d02809eff1f5da79c208f3460c805bf
Lauri <lauri.nomtak@mail.ee>
2015-09-08 10:17:28 +0300

    {summar} [Delivers #{number}]

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="assets/stylesheet.css">
+    <meta charset="UTF-8">
+    <title>Allar Kikkas</title>
+</head>
+<body>
+<h1>Allari tutvustus</h1>
+<ul>
+    <li>Nimi: Allar Kikkas </li>
+    <li>Vanus: 20</li>
+    <li>Elukoht: Tartu</li>
+    <li>Hobid: Foto ja videograafia</li>
+</ul>
+</body>
+</html>
