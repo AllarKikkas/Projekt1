707efaa5de2f2b42be56f6488d0e233e78194770
Lauri <lauri.nomtak@mail.ee>
2015-09-16 13:37:31 +0300

    Lisasin kujunduse ja eluloo. [Delivers #103489842]

--- index.php
+++ index.php
@@ -1,19 +1,91 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" type="text/css" href="assets/stylesheet.css">
+    <link rel="stylesheet" type="text/css" href=stylesheet.css>
     <meta charset="UTF-8">
     <title>Allar Kikkas</title>
 </head>
 <body>
-<h1>Allari tutvustus</h1>
-<ul>
-    <li>Nimi: Allar Kikkas </li>
-    <li>Vanus: 20</li>
-    <li>Elukoht: Tartu</li>
-    <li>Hobid: Foto ja videograafia</li>
-</ul>
-<a href="https://www.facebook.com/KikkasAllar">Facebook</a>
-<a href="https://www.youtube.com/channel/UCVxhjoug8xAF50XaNN181Qg">Youtube</a>
+<!-- kommentaar -->
+<div id="header">
+    <h1>Allari tutvustus</h1>
+</div>
+<div id="taust">
+    <div id="tabel">
+        <img
+            src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xaf1/v/t1.0-1/c210.0.540.540/10645022_10203966779388365_1506081348100776491_n.jpg?oh=9bf75d45a9fe51d3020876e0ca04a840&oe=566669A8&__gda__=1448994973_baa74a83bb5c8b14df82855da1f55b6d">
+        <table>
+            <tr>
+                <th colspan="2" style=" font-weight: bold; padding: 20px; color: white; background-color: midnightblue;">&Uumlldandmed</th>
+            </tr>
+            <tr>
+                <ul>
+                <td class="item"> <li>Nimi:</li> </td>
+                <td class="value">Allar Kikkas</td>
+            </tr>
+            <tr>
+                <td class="item"> <li>Vanus:</li> </td>
+                <td class="value">20</td>
+            </tr>
+            <tr>
+                <td class="item"> <li>Elukoht:</li> </td>
+                <td class="value">Tartu</td>
+            </tr>
+            <tr>
+                <td class="item"> <li>Hobid:</li> </ul></td>
+                <td class="value">
+                    <ol>
+                        <li>Fotograafia</li>
+                        <li>Videograafia</li>
+                    </ol>
+                </td>
+            </tr>
+        </table>
+
+    </div>
+    <br>
+    <br>
+
+    <div id="alumine">
+        <div id="lingid">
+            <h3>Lingid</h3>
+            <ul>
+                <li>
+                    <a id=fb href="https://www.facebook.com/KikkasAllar"><img src="assets/img/fb.png" align="middle"></a>
+                </li>
+                <li>
+                    <a id=yt href="https://www.youtube.com/channel/UCVxhjoug8xAF50XaNN181Qg"><img src="assets/img/yt.png" align="middle"></a>
+                </li>
+            </ul>
+        </div>
+        <div id="elulugu">
+            <h2>Elulugu</h2>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur possimus, vel. Accusantium architecto
+                aspernatur harum impedit inventore libero, maxime nam nemo officia pariatur qui quos tempore! Dolorem,
+                laboriosam voluptatibus. Nobis!</p>
+        </div>
+    </div>
+
+    <hr>
+    <p style="text-align: center;">2015</p>
+
+</div>
+
 </body>
 </html>
+
+
+<!--<ul>
+    <li>Nimi: Allar Kikkas</li>
+    <br>
+    <li>Vanus: 20</li>
+    <br>
+    <li>Elukoht: Tartu</li>
+    <br>
+    <li>Hobid:
+        <ol>
+            <li>Fotograafia</li>
+            <li>Videograafia</li>
+        </ol>
+</ul> -->
